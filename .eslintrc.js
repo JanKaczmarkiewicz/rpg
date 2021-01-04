6f60e8b63becbae4b399d1155865a7863de1fda1
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-04 21:56:38 +0100

    feat: eslint prettier fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,10 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    project: "./tsconfig.json",
-  },
-  extends: ["eslint:recommended", "airbnb-typescript", "prettier"],
-  plugins: [
-    "prettier",
-    "@typescript-eslint/recommended",
-    "@typescript-eslint/recommended-requiring-type-checking",
-  ],
-  rules: {
-    "prettier/prettier": ["error"],
-  },
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    extends: ['prettier', 'prettier/@typescript-eslint'],
+    plugins: ['prettier'],
 };
