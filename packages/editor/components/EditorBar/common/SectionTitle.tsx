567342156bdecddd5e7d9b9bfb924402e6995ef9
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-06 12:47:24 +0100

    feat: SectionTitle atom

---
+++ packages/editor/components/EditorBar/common/SectionTitle.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@material-ui/core';
+import { FunctionComponent } from 'react';
+
+const SectionTitle: FunctionComponent = ({ children }) => <Typography variant="h5">{children}</Typography>;
+
+export default SectionTitle;
