340d9849c92c59809036dfb50d73c8fb6f99e219
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-06 13:17:04 +0100

    refactor: remove unused variable

--- packages/editor/components/EditorBar/EditorBar.tsx
+++ packages/editor/components/EditorBar/EditorBar.tsx
@@ -2,6 +2,8 @@ import { Box } from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
 import MainMenuBlade from './Blades/MainManuBlade';
 
+const DRAWER_WIDTH = 240;
+
 const EditorBar: FunctionComponent = () => {
     return (
         <Box p={2}>
