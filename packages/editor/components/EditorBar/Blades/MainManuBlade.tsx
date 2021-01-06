ff82bd0dc22a0f7017a254425f9116bdda8c2299
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-06 13:16:31 +0100

    feat: main menu options icons added

--- packages/editor/components/EditorBar/Blades/MainManuBlade.tsx
+++ packages/editor/components/EditorBar/Blades/MainManuBlade.tsx
@@ -1,12 +1,19 @@
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import React, { ReactNode } from 'react';
+import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import {
+    Layers as LayersIcon,
+    EmojiPeople as EmojiPeopleIcon,
+    ViewQuilt as ViewQuiltIcon,
+    SettingsInputSvideo as SettingsInputSvideoIcon,
+} from '@material-ui/icons';
 import { LocalizeKey, useLocalize } from '../../../localization/useLocalize';
 import SectionTitle from '../common/SectionTitle';
 
-const options: { optionTextKey: LocalizeKey }[] = [
-    { optionTextKey: 'editLayers' },
-    { optionTextKey: 'editWalls' },
-    { optionTextKey: 'editCharacters' },
-    { optionTextKey: 'editNPCs' },
+const options: { optionTextKey: LocalizeKey; icon: ReactNode }[] = [
+    { optionTextKey: 'editLayers', icon: <LayersIcon /> },
+    { optionTextKey: 'editWalls', icon: <ViewQuiltIcon /> },
+    { optionTextKey: 'editCharacters', icon: <SettingsInputSvideoIcon /> },
+    { optionTextKey: 'editNPCs', icon: <EmojiPeopleIcon /> },
 ];
 
 const MainMenuBlade = () => {
@@ -15,8 +22,9 @@ const MainMenuBlade = () => {
         <>
             <SectionTitle>{localize('whatYouWantToDo')}</SectionTitle>
             <List>
-                {options.map(({ optionTextKey }) => (
+                {options.map(({ optionTextKey, icon }) => (
                     <ListItem button key={optionTextKey}>
+                        <ListItemIcon>{icon}</ListItemIcon>
                         <ListItemText primary={localize(optionTextKey)} />
                     </ListItem>
                 ))}
