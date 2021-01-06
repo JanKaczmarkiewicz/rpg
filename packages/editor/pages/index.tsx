1bf4daf80347e12a0898ab02a96ac0e0cd1af217
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-06 13:18:53 +0100

    fix: disable ssr for main route

--- packages/editor/pages/index.tsx
+++ packages/editor/pages/index.tsx
@@ -1,30 +1,34 @@
-import { AppBar, Box, Drawer, Toolbar } from "@material-ui/core";
-import Head from "next/head";
-import React from "react";
-import EditedMap from "../components/EditedMap/EditedMap";
-import EditorBar from "../components/EditorBar/EditorBar";
+import { AppBar, Box, Drawer, Toolbar } from '@material-ui/core';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import React from 'react';
+import EditedMap from '../components/EditedMap/EditedMap';
+import EditorBar from '../components/EditorBar/EditorBar';
 
-const drawerWidth = 240;
+function Home() {
+    return (
+        <div>
+            <Head>
+                <title>RPG editor</title>
+            </Head>
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>RPG editor</title>
-      </Head>
+            <AppBar position="fixed">
+                <Toolbar>RPG editor</Toolbar>
+            </AppBar>
 
-      <AppBar position="fixed">
-        <Toolbar>RPG editor</Toolbar>
-      </AppBar>
+            <Drawer open variant="permanent">
+                <EditorBar />
+            </Drawer>
 
-      <Drawer open variant="permanent">
-        <EditorBar />
-      </Drawer>
-
-      <Box>
-        <Toolbar />
-        <EditedMap />
-      </Box>
-    </div>
-  );
+            <Box>
+                <Toolbar />
+                <EditedMap />
+            </Box>
+        </div>
+    );
 }
+
+// TODO: disabling ssr for now becouse it couses mismatch in css classes. See: https://material-ui.com/guides/server-rendering/
+export default dynamic(() => Promise.resolve(Home), {
+    ssr: false,
+});
