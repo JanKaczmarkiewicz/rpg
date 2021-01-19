f631a18d09fbf952d1a6a6c32fe3986719759229
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-19 23:20:06 +0100

    feat: app header

---
+++ packages/editor/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { FunctionComponent } from 'react';
+
+const AppHeader: FunctionComponent = () => {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h6">Map editor</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default AppHeader;
--- packages/editor/pages/index.tsx
+++ packages/editor/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
-import { AppBar, Box, Drawer, Toolbar } from '@material-ui/core';
+import { Box, Toolbar } from '@material-ui/core';
 
 import EditedMap from '../components/EditedMap/EditedMap';
-import EditorBar from '../components/EditorBar/EditorBar';
+import Header from '../components/AppHeader/AppHeader';
+import LeftMenu from '../components/LeftMenu/LeftMenu';
 
 function Home() {
     return (
@@ -12,24 +13,16 @@ function Home() {
             <Head>
                 <title>RPG editor</title>
             </Head>
-
-            <AppBar position="fixed">
-                <Toolbar>RPG editor</Toolbar>
-            </AppBar>
-
-            <Drawer open variant="permanent">
-                <EditorBar />
-            </Drawer>
-
-            <Box>
-                <Toolbar />
+            <Header />
+            <LeftMenu />
+            <main>
                 <EditedMap />
-            </Box>
+            </main>
         </div>
     );
 }
 
-// TODO: disabling ssr for now becouse it couses mismatch in css classes. See: https://material-ui.com/guides/server-rendering/
+// TODO: disabling ssr for now because it causes mismatch in css classes. See: https://material-ui.com/guides/server-rendering/
 export default dynamic(() => Promise.resolve(Home), {
     ssr: false,
 });
