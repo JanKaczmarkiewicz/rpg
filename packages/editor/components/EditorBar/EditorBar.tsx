b5c2ddd86c7f040d46793bbaa20bb5e156f14378
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-01-20 18:53:50 +0100

    fix: remove constant

--- packages/editor/components/EditorBar/EditorBar.tsx
+++ packages/editor/components/EditorBar/EditorBar.tsx
@@ -2,8 +2,6 @@ import { Box } from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
 import MainMenuBlade from './Blades/MainManuBlade';
 
-const DRAWER_WIDTH = 240;
-
 const EditorBar: FunctionComponent = () => {
     return (
         <Box p={2}>
