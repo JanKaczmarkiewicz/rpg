e5a9f249e5015f3fda3f3ce32b7f64b625bcf4f8
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-02-13 16:04:49 +0100

    fix: getMaps on get /

--- packages/backend/src/routes/maps/index.ts
+++ packages/backend/src/routes/maps/index.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import deleteMap, { validateDeleteMapParams } from './deleteMap';
 import getMap, { validateGetMapParams } from './getMap';
 import createMap, { validateCreateMapBody } from './createMap';
+import getMaps from './getMaps';
 
 const router = Router();
 
-router.get('/', getMap);
+router.get('/', getMaps);
 router.get('/:id', validateGetMapParams, getMap);
 router.post('/', validateCreateMapBody, createMap);
 router.delete('/:id', validateDeleteMapParams, deleteMap);
