78417fca158c6cb0072dddbd82d6f9784b65e731
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-03-07 17:53:57 +0100

    refactor: remove console.log

--- packages/editor/components/EditorBar/blades/EnemiesEditor/EnemiesEditor.tsx
+++ packages/editor/components/EditorBar/blades/EnemiesEditor/EnemiesEditor.tsx
@@ -23,7 +23,6 @@ const EnemiesEditor: FunctionComponent = () => {
     const { data = [] } = useQuery('enemies', client.enemy.getMany);
 
     const onSelectCharacter = (enemy: EnemyObjectResponse) => {
-        console.log(enemy);
         dispatch(setEditorSelectedEnemy({ enemy }));
     };
 
